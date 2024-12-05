5b51641eda171ab7be8b3684e585b6bcee86f069
lifpro <ysalissou@gmail.com>
2024-12-05 16:59:32 +0000

    ree

--- test/test.php
+++ test/test.php
@@ -1,3 +1,14 @@
-<?php
-$nom = "Fatou";
-echo "Bonjour, $nom bienvenue sur notre site.";
+<html>
+
+<head>
+    <title>Bienvenue sur notre site</title>
+</head>
+
+<body>
+
+    <h1>Technolab ISTA</h1>
+    <?php $nom = "Fatou" ?>
+    <p>Bonjour, <?php echo $nom ?> bienvenue sur notre site.</p>
+</body>
+
+</html>
